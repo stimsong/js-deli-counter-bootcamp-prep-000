10ba67b93d99f20cf228d027acaeb4bece51440a
Greg Stimson <stimsong@yahoo.com>
2018-01-21 00:31:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (!katzDeliLine.length) {
-    return "The line is currently empty";
+    return "The line is currently empty.";
   }
   
   var currLineNumAndName = [];
